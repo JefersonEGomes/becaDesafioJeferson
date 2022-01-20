4f5cc47e3694c11468cae62ab99106d8e011f38d
JefersonEGomes <jefersonsportsempre@gmail.com>
2022-01-19 21:05:59 -0300

    Adicionando arquivos iniciais para o desafio

---
+++ src/main/java/com/becaJavaJeferson/BecaJavaJefersonApplication.java
@@ -0,0 +1,13 @@
+package com.becaJavaJeferson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecaJavaJefersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecaJavaJefersonApplication.class, args);
+	}
+
+}

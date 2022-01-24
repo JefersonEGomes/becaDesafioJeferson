2839937e9051b612494da26496006462273db7dc
JefersonEGomes <jefersonsportsempre@gmail.com>
2022-01-24 17:58:06 -0300

    2 correção Controllers

--- src/main/java/com/becaJavaJeferson/controllers/LocacaoController.java
+++ src/main/java/com/becaJavaJeferson/controllers/LocacaoController.java
@@ -1,22 +1,26 @@
 package com.becaJavaJeferson.controllers;
 
 import com.becaJavaJeferson.model.Locacao;
+import com.becaJavaJeferson.model.Locador;
+import com.becaJavaJeferson.model.Locatario;
+import com.becaJavaJeferson.model.Produto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Random;
-import java.util.ResourceBundle;
 
 @RestController
 @RequestMapping("/locacao")
 public class LocacaoController {
 
+
     //CREATE
     @PostMapping
     public ResponseEntity<Locacao> criar(@RequestBody Locacao locacao) {
         Random id = new Random();
-        locacao.setIdLocacao(id.nextInt(1000));
+        locacao.setId(id.nextInt(1000));
 
         return ResponseEntity.created(null).body(locacao);
     }
@@ -24,39 +28,78 @@ public class LocacaoController {
     //READ
     @GetMapping
     public ResponseEntity<List<Locacao>> listar(){
+        // Instancia para Teste
+
+        Locador locador1 = new Locador();
+        locador1.setId(1);
+        locador1.setNome("João");
+        locador1.setIdade(22);
+        locador1.setCpf(12345678900L);
+        locador1.setTelefone(81945678900L);
+
+        Locatario locatario1 = new Locatario();
+        locatario1.setId(2);
+        locatario1.setNome("Rodrigo");
+        locatario1.setIdade(25);
+        locatario1.setCpf(12345678900L);
+        locatario1.setTelefone(81945678900L);
+
+        Produto produto1 = new Produto();
+        produto1.setId(3);
+        produto1.setNome("Controle Xbox");
+        produto1.setCategoria("Tecnologia");
+        produto1.setPreco(250.00);
+        produto1.setLocador(locador1);
+        produto1.setId(58);
+
         Locacao locacao1 = new Locacao();
-        Locacao locacao2 = new Locacao();
 
-        locacao1.setIdLocacao(1);
-        locacao1.setIdLocador(2);
-        locacao1.setIdLocatario(3);
-        locacao1.setIdProduto(5);
-        locacao1.setDataLocacao("05/01/2022");
-        locacao1.setDataDevolve("14/03/2022");
-
-        locacao2.setIdLocacao(2);
-        locacao2.setIdLocador(1);
-        locacao2.setIdLocatario(5);
-        locacao2.setIdProduto(3);
-        locacao2.setDataLocacao("01/05/2022");
-        locacao2.setDataDevolve("03/07/2022");
+        locacao1.setProduto(produto1);
+        locacao1.setId(2);
+        locacao1.setLocador(locador1);
+        locacao1.setLocatario(locatario1);
+        locacao1.setDataAluguel(LocalDate.of(2022,8,10));
+        locacao1.setDataDevolve(LocalDate.of(2022,5,23));
 
         return ResponseEntity.ok(List.of(
-                locacao1,
-                locacao2
+                locacao1
         ));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Locacao> obter(@PathVariable Integer id){
+        // Instancia para teste
+        Locador locador3 = new Locador();
+        locador3.setId(3);
+        locador3.setNome("Jorge");
+        locador3.setIdade(35);
+        locador3.setCpf(12345678900L);
+        locador3.setTelefone(81945678900L);
+
+        Locatario locatario3 = new Locatario();
+        locatario3.setId(3);
+        locatario3.setNome("Richarlison");
+        locatario3.setIdade(25);
+        locatario3.setCpf(12365478900L);
+        locatario3.setTelefone(81912378900L);
+
+        Produto produto3 = new Produto();
+        produto3.setId(3);
+        produto3.setNome("Controle Xbox");
+        produto3.setCategoria("Tecnologia");
+        produto3.setPreco(250.00);
+        produto3.setLocador(locador3);
+        produto3.setId(58);
+
+
         Locacao locacao3 = new Locacao();
 
-        locacao3.setIdLocacao(id);
-        locacao3.setIdLocador(2);
-        locacao3.setIdLocatario(3);
-        locacao3.setIdProduto(5);
-        locacao3.setDataLocacao("08/02/2022");
-        locacao3.setDataDevolve("16/04/2022");
+        locacao3.setProduto(produto3);
+        locacao3.setId(2);
+        locacao3.setLocador(locador3);
+        locacao3.setLocatario(locatario3);
+        locacao3.setDataAluguel(LocalDate.of(2022,5,22));
+        locacao3.setDataDevolve(LocalDate.of(2022,10,3));
 
         return ResponseEntity.ok(locacao3);
     }
@@ -64,7 +107,7 @@ public class LocacaoController {
     //UPDATE
     @PatchMapping("/{id}")
     public ResponseEntity<Locacao> atualizar(@RequestBody Locacao locacao,@PathVariable Integer id){
-        locacao.setIdLocacao(id);
+        locacao.setId(id);
 
         return ResponseEntity.ok(locacao);
     }
--- src/main/java/com/becaJavaJeferson/controllers/ProdutoController.java
+++ src/main/java/com/becaJavaJeferson/controllers/ProdutoController.java
@@ -1,5 +1,6 @@
 package com.becaJavaJeferson.controllers;
 
+import com.becaJavaJeferson.model.Locador;
 import com.becaJavaJeferson.model.Produto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,13 +11,12 @@ import java.util.Random;
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
-
     //CREATE
     @PostMapping
     public ResponseEntity<Produto> criar(@RequestBody Produto produto){
 
         Random randomId = new Random();
-        produto.setIdProduto(randomId.nextInt(1000));
+        produto.setId(randomId.nextInt(1000));
 
         if( produto.getNome().length() <= 3){
             return ResponseEntity.unprocessableEntity().build();
@@ -29,27 +29,50 @@ public class ProdutoController {
     // READ
     @GetMapping
     public ResponseEntity<List<Produto>> listar(){
+        // Instancia para Teste
+        Locador locador1 = new Locador();
+        locador1.setId(1);
+        locador1.setNome("João");
+        locador1.setIdade(22);
+        locador1.setCpf(12345678900L);
+        locador1.setTelefone(81945678900L);
+
+        Locador locador2 = new Locador();
+        locador2.setId(2);
+        locador2.setNome("Roberto");
+        locador2.setIdade(34);
+        locador2.setCpf(90087654321L);
+        locador2.setTelefone(81909876543L);
+
+        Locador locador3 = new Locador();
+        locador1.setId(3);
+        locador1.setNome("Elias");
+        locador1.setIdade(52);
+        locador1.setCpf(56912300000L);
+        locador1.setTelefone(81912345678L);
+
         Produto prod1 = new Produto();
-        prod1.setIdProduto(1);
+        prod1.setId(1);
         prod1.setNome("Controle Xbox");
         prod1.setCategoria("Tecnologia");
         prod1.setPreco(250.00);
-        prod1.setIdLocador(58);
+        prod1.setId(58);
+        prod1.setLocador(locador1);
 
 
         Produto prod2 = new Produto();
-        prod2.setIdProduto(2);
+        prod2.setId(2);
         prod2.setNome("Bicicleta");
         prod2.setCategoria("Veículo");
         prod2.setPreco(320.00);
-        prod2.setIdLocador(27);
+        prod2.setLocador(locador2);
 
         Produto prod3 = new Produto();
-        prod3.setIdProduto(3);
+        prod3.setId(3);
         prod3.setNome("Bola de Futebol");
         prod3.setCategoria("Esportes");
         prod3.setPreco(50.00);
-        prod3.setIdLocador(10);
+        prod3.setLocador(locador3);
 
 
         return ResponseEntity.ok(List.of(
@@ -62,12 +85,21 @@ public class ProdutoController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Produto> obter(@PathVariable Integer id){
+        // Instancia para Teste
+        Locador locador1 = new Locador();
+        locador1.setId(1);
+        locador1.setNome("João");
+        locador1.setIdade(22);
+        locador1.setCpf(12345678900L);
+        locador1.setTelefone(81945678900L);
+
         Produto prod1 = new Produto();
-        prod1.setIdProduto(id);
+        prod1.setId(id);
         prod1.setNome("Controle Xbox");
         prod1.setCategoria("Tecnologia");
         prod1.setPreco(250.00);
-        prod1.setIdLocador(58);
+        prod1.setLocador(locador1);
+        prod1.setId(58);
 
         return ResponseEntity.ok(prod1);
     }
@@ -75,7 +107,7 @@ public class ProdutoController {
     // UPDATE
     @PatchMapping("/{id}")
     public ResponseEntity<Produto> atualizar(@RequestBody Produto produto, @PathVariable Integer id){
-        produto.setIdProduto(id);
+        produto.setId(id);
 
         return ResponseEntity.ok(produto);
     }
